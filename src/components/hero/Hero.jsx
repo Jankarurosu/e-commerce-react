202f47108c64effc025aa4b05060d8c0ef610b03
Jankarurosu <103855697+Jankarurosu@users.noreply.github.com>
2024-05-27 14:08:52 -0500

    Implementando el modo responsivo en la sección de nuevas colecciones y actualizando el modo responsivo de la sección hero y popular

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -10,11 +10,11 @@ export const Hero = () => {
                 <h2>NEW ARRIVALS ONLY</h2>
                 <div>
                     <div className="hero-hand-icon">
-                        <p>new</p>
+                        <p>New</p>
                         <img src={hand_icon} alt="hand_icon" />
                     </div>
-                    <p>collections</p>
-                    <p>for everyone</p>
+                    <p>Collections</p>
+                    <p>For Everyone</p>
                 </div>
                 <div className='hero-latest-btn'>
                     <div>Latest Collection</div>
