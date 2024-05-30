57d7cf3058637808b181d2d48df484a9f7f7ae72
Jankarurosu <103855697+Jankarurosu@users.noreply.github.com>
2024-05-30 15:01:02 -0500

    Implementa el modo responsivo en ShopCategory

--- src/pages/ShopCategory.jsx
+++ src/pages/ShopCategory.jsx
@@ -10,7 +10,6 @@ export const ShopCategory = (props) => {
 
     return (
         <div className="shop-category">
-            <img className='shopcategory-banner' src={props.banner} alt="" />
             <div className="shopcategory-indexSort">
                 <p>
                     <span>Showing 1-12</span> out of 36 products
